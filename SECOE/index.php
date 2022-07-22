c898a5acbf081731e919ad5d82f5e39097b467cb
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2022-07-22 16:51:50 -0600

    vaciado

--- SECOE/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "controladores/hackathon.controlador.php";
-require_once "modelos/hackathon.modelo.php";
-require_once "modelos/rutas.php";
-$plantilla = new controladorHackathon();
-$plantilla -> plantilla();
--- SECOE/modelos/rutas.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class ruta{
-	static public function ctrRuta(){
-		return "";
-	}
-}
