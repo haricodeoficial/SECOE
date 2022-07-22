ee90828c03eb3a581a9d8af03d8bf8db801f20d7
JasonZamora <67882782+JasonZamora@users.noreply.github.com>
2022-07-22 14:48:46 -0600

    Add files via upload

---
+++ SECOE/vistas/modulos/conoce-mas.php
@@ -0,0 +1,58 @@
+
+<div class="container-fluid bannerConoce">
+  <h1>Cambio climatico</h1>
+</div>
+
+
+
+
+    <div width="100%" class="intro" >
+      <h1>
+        CO2
+      </h1>
+
+<div data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine">
+
+     <p>preube</p>
+</div>
+
+<div data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine">
+
+     <p>ueube</p>
+</div>
+
+
+          <video src="<?php echo $url;?>vistas/img/CO2.mp4"></video>
+    </div>
+
+  <section class="sectionConocer">
+      <h1>REVOLUTIONARRY</h1>
+  </section>
+  <!--===========================
+    JavaScript  
+  =============================-->
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"
+      integrity="sha256-2p2tRZlPowp3P/04Pw2rqVCSbhyV/IB7ZEVUglrDS/c="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.js"
+      integrity="sha256-peenofh8a9TIqKdPKIeQE7mJvuwh+J0To7nslvpj1jI="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.js"
+      integrity="sha256-31FC/OT6XpfjAhj9FuXjw5/wPXXawCAjJQ29E23/XPk="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"
+      integrity="sha256-lPE3wjN2a7ABWHbGz7+MKBJaykyzqCbU96BJWjio86U="
+      crossorigin="anonymous"
+    ></script>
+   
